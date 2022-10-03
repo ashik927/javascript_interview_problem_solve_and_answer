0cc53e5b134b0ed62a6c802f6c7d5c041470ad40
Khandker Ashik Mahmud <67101387+ashik927@users.noreply.github.com>
2022-10-03 15:14:03 +0600

    generating  a random number

---
+++ 9==randomNumber.js
@@ -0,0 +1,19 @@
+// @ashik927 && @ashik1012
+
+// generating  a random number
+
+const random = Math.random();
+console.log(random);
+
+
+// Integer Value between Two Numbers
+
+// input from the user
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
+// generating a random number
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
