7ac9ed2b94f86d603f3c56d4d2dd0cfab504857a
Khandker Ashik Mahmud <67101387+ashik927@users.noreply.github.com>
2022-10-03 15:17:29 +0600

    check if the number is even or odd

---
+++ 10==checkOddOrEven.js
@@ -0,0 +1,24 @@
+
+// @ashik927 && @ashik1012
+
+// Using if...else
+
+// program to check if the number is even or odd
+// take input from the user
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+
+
+// Using Ternary Operator
+
+
