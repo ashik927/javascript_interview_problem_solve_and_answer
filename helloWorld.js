60a6550887cefc3dabda1d623e3385d844684b03
Khandker Ashik Mahmud <67101387+ashik927@users.noreply.github.com>
2022-10-03 14:03:51 +0600

    first problem hello world different types

---
+++ helloWorld.js
@@ -0,0 +1,24 @@
+// JavaScript Program To Print Hello World
+// We will use these three ways to print 'Hello, World!'.
+
+// console.log()
+// alert()
+// document.write()
+
+
+// console.log() is used in debugging the code.
+
+// the hello world program
+console.log('Hello World');
+
+
+// The alert() method displays an alert box over the current window with the specified
+
+// the hello world program
+alert("Hello, World!");
+
+
+// document.write() is used when you want to print the content to the HTML document.
+
+// the hello world program
+document.write('Hello, World!');
